a5b59177ac026d0879028ba91bda2b8b393a9bb7
Chona villamor <118464458+onanguniverse@users.noreply.github.com>
2023-07-04 02:43:25 +0800

    Add files via upload

---
+++ sinnedproject/Form2.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form2
+
+    Dim connection As New SqlConnection("Server=DESKTOP-MLUEKGP\SQLEXPRESS; Database = project; Integrated Security = true")
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim command As New SqlCommand("insert into Users(Fname, Lname, age) values(@fn,@ln,@age)", connection)
+
+        command.Parameters.Add("@fn", SqlDbType.VarChar).Value = TextBox1.Text
+        command.Parameters.Add("@ln", SqlDbType.VarChar).Value = TextBox2.Text
+        command.Parameters.Add("@age", SqlDbType.Int).Value = TextBox3.Text
+
+        connection.Open()
+        If command.ExecuteNonQuery() = 1 Then
+
+            MessageBox.Show("New User Added")
+
+        Else
+
+            MessageBox.Show("User Not Added")
+
+        End If
+
+        connection.Close()
+
+    End Sub
+End Class
---
+++ sinnedproject/Module1.vb
@@ -0,0 +1,8 @@
+﻿Imports System.Data.SqlClient
+Module Module1
+    Public con As New SqlConnection
+    Public cmd As New SqlCommand
+    Sub opencon()
+        con.ConnectionString = "Data Source=DESKTOP-MLUEKGP\SQLEXPRESS;Initial Catalog=softwaredesign;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+    End Sub
+End Module
